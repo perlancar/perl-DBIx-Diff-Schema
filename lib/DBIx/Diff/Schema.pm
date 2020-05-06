a44404167d00ae330d25d3cf10670ceab4934e1f
perlancar (on pc-home) <perlancar@gmail.com>
2020-05-06 09:47:52 +0700

    [removed] Remove list_columns, list_tables, list_{table_,}indexes, check_table_exists; move those to DBIx::Util::Schema

--- lib/DBIx/Diff/Schema.pm
+++ lib/DBIx/Diff/Schema.pm
@@ -10,16 +10,12 @@ use strict 'subs', 'vars';
 use warnings;
 use Log::ger;
 
+use DBIx::Util::Schema qw(has_table list_tables list_columns);
 use List::Util qw(first);
 
 use Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
-                       list_columns
-                       list_tables
-                       list_table_indexes
-                       list_indexes
-                       check_table_exists
                        diff_db_schema
                        diff_table_schema
                        db_schema_eq
@@ -80,252 +76,6 @@ my %diff_table_args = (
         pos => 3,
     },
 );
-
-$SPEC{check_table_exists} = {
-    v => 1.1,
-    summary => 'Check whether a table exists',
-    args => {
-        %arg0_dbh,
-        %arg1_table,
-    },
-    args_as => "array",
-    result_naked => 1,
-};
-sub check_table_exists {
-    my ($dbh, $name) = @_;
-    my $sth;
-    if ($name =~ /(.+)\.(.+)/) {
-        $sth = $dbh->table_info(undef, $1, $2, undef);
-    } else {
-        $sth = $dbh->table_info(undef, undef, $name, undef);
-    }
-
-    $sth->fetchrow_hashref ? 1:0;
-}
-
-$SPEC{list_tables} = {
-    v => 1.1,
-    summary => 'List table names in a database',
-    args => {
-        %arg0_dbh,
-    },
-    args_as => "array",
-    result_naked => 1,
-};
-sub list_tables {
-    my ($dbh) = @_;
-
-    my $driver = $dbh->{Driver}{Name};
-
-    my @res;
-    my $sth = $dbh->table_info(undef, undef, undef, undef);
-    while (my $row = $sth->fetchrow_hashref) {
-        my $name  = $row->{TABLE_NAME};
-        my $schem = $row->{TABLE_SCHEM};
-        my $type  = $row->{TABLE_TYPE};
-
-        if ($driver eq 'mysql') {
-            # mysql driver returns database name as schema, so that's useless
-            $schem = '';
-        }
-
-        next if $type eq 'VIEW';
-        next if $type eq 'INDEX';
-        next if $schem =~ /^(information_schema)$/;
-
-        if ($driver eq 'Pg') {
-            next if $schem =~ /^(pg_catalog)$/;
-        } elsif ($driver eq 'SQLite') {
-            next if $schem =~ /^(temp)$/;
-            next if $name =~ /^(sqlite_master|sqlite_temp_master)$/;
-        }
-
-        push @res, join(
-            "",
-            $schem,
-            length($schem) ? "." : "",
-            $name,
-        );
-    }
-    sort @res;
-}
-
-$SPEC{list_indexes} = {
-    v => 1.1,
-    summary => 'List indexes for a table in a database',
-    description => <<'_',
-
-SQLite notes: information is retrieved from DBI's table_info(). Autoindex for
-primary key is not listed using table_info(), but this function adds it by
-looking at `sqlite_master` table.
-
-MySQL notes: information is retrieved from statistics_info(). Note that a
-multi-column index is reported as separate rows by statistics_info(), one for
-each indexed column. But this function merges them and returns the list of
-columns in `columns`.
-
-_
-    args => {
-        %arg0_dbh,
-        %arg1_table,
-    },
-    args_as => "array",
-    result_naked => 1,
-};
-sub list_indexes {
-    my ($dbh, $wanted_table) = @_;
-
-    my $driver = $dbh->{Driver}{Name};
-
-    my @res;
-
-    if ($driver eq 'SQLite') {
-
-        my @wanted_tables;
-        if (defined $wanted_table) {
-            @wanted_tables = ($wanted_table);
-        } else {
-            @wanted_tables = list_tables($dbh);
-        }
-        for (@wanted_tables) { $_ = $1 if /.+\.(.+)/ }
-
-        my $sth = $dbh->prepare("SELECT * FROM sqlite_master");
-        $sth->execute;
-        while (my $row = $sth->fetchrow_hashref) {
-            next unless $row->{type} eq 'index';
-            next unless grep { $_ eq $row->{tbl_name} } @wanted_tables;
-            next unless $row->{name} =~ /\Asqlite_autoindex_.+_(\d+)\z/;
-            my $col_num = $1;
-            my @cols = list_columns($dbh, $row->{tbl_name});
-            push @res, {
-                name      => "PRIMARY",
-                table     => $row->{tbl_name},
-                columns   => [$cols[$col_num-1]{COLUMN_NAME}],
-                is_unique => 1,
-                is_pk     => 1,
-            };
-        }
-
-        $sth = $dbh->table_info(undef, undef, undef, undef);
-      ROW:
-        while (my $row = $sth->fetchrow_hashref) {
-            next unless $row->{TABLE_TYPE} eq 'INDEX';
-
-            my $table = $row->{TABLE_NAME};
-            my $schem = $row->{TABLE_SCHEM};
-
-            # match table name
-            if (defined $wanted_table) {
-                if ($wanted_table =~ /(.+)\.(.+)/) {
-                    next unless $schem eq $1 && $table eq $2;
-                } else {
-                    next unless $table eq $wanted_table;
-                }
-            }
-
-            next unless my $sql = $row->{sqlite_sql};
-            #use DD; dd $row;
-            $sql =~ s/\A\s*CREATE\s+(UNIQUE\s+)?INDEX\s+//is or do {
-                log_trace "Not a CREATE INDEX statement, skipped: $row->{sqlite_sql}";
-                next ROW;
-            };
-
-            $row->{is_unique} = $1 ? 1:0; # not-standard, backward compat
-            $row->{NON_UNIQUE} = $1 ? 0:1;
-
-            $sql =~ s/\A(\S+)\s+//s
-                or die "Can't extract index name from sqlite_sql: $sql";
-            $row->{name} = $1; # non-standard, backward compat
-            $row->{INDEX_NAME} = $1;
-
-            $sql =~ s/\AON\s*(\S+)\s*\(\s*(.+)\s*\)//s
-                or die "Can't extract indexed table+columns from sqlite_sql: $sql";
-            $row->{table} = $table // $1; # non-standard, backward-compat
-            $row->{columns} = [split /\s*,\s*/, $2]; # non-standard
-
-            push @res, $row;
-        } # while row
-
-    } elsif ($driver eq 'mysql') {
-
-        my $sth = $dbh->statistics_info(undef, undef, undef, undef, undef);
-        $sth->execute;
-        my @res0;
-        while (my $row = $sth->fetchrow_hashref) {
-            if (defined $wanted_table) {
-                if ($wanted_table =~ /(.+)\.(.+)/) {
-                    next unless $row->{TABLE_SCHEM} eq $1 && $row->{TABLE_NAME} eq $2;
-                } else {
-                    next unless $row->{TABLE_NAME} eq $wanted_table;
-                }
-            }
-            $row->{table} = $row->{TABLE_NAME}; # non-standard, backward-compat
-            $row->{name} = $row->{INDEX_NAME}; # non-standard, backward-compat
-            $row->{is_unique} = $row->{NON_UNIQUE} ? 0:1; # non-standard, backward-compat
-            $row->{is_pk} = $row->{INDEX_NAME} eq 'PRIMARY' ? 1:0; # non-standard, backward-compat
-
-            push @res0, $row;
-        }
-
-        # merge separated per-indexed-column result into a single all-columns
-        # result
-        my @index_names;
-        for my $row (@res0) { push @index_names, $row->{INDEX_NAME} unless grep { $row->{INDEX_NAME} eq $_ } @index_names }
-        for my $index_name (@index_names) {
-            my @hashes = grep { $_->{INDEX_NAME} eq $index_name } @res0;
-            if (@hashes == 1) {
-                push @res, $hashes[0];
-            } else {
-                my %merged_hash;
-                $merged_hash{columns} = [];
-                for my $hash (@hashes) {
-                    $merged_hash{columns}[ $hash->{ORDINAL_POSITION}-1 ] = $hash->{COLUMN_NAME};
-                    for (keys %$hash) { $merged_hash{$_} = $hash->{$_} }
-                }
-                delete $merged_hash{ORDINAL_POSITION};
-                push @res, \%merged_hash;
-            }
-        }
-
-    } else {
-
-        die "Driver $driver is not yet supported for list_indexes";
-    }
-
-    @res;
-}
-
-# old name, deprecated
-$SPEC{list_table_indexes} = $SPEC{list_indexes};
-*list_table_indexes = \&list_indexes;
-
-$SPEC{list_columns} = {
-    v => 1.1,
-    summary => 'List columns of a table',
-    args => {
-        %arg0_dbh,
-        %arg1_table,
-    },
-    args_as => "array",
-    result_naked => 1,
-};
-sub list_columns {
-    my ($dbh, $table) = @_;
-
-    my @res;
-    my ($schema, $utable);
-    if ($table =~ /\./) {
-        ($schema, $utable) = split /\./, $table;
-    } else {
-        $schema = undef;
-        $utable = $table;
-    }
-    my $sth = $dbh->column_info(undef, $schema, $utable, undef);
-    while (my $row = $sth->fetchrow_hashref) {
-        push @res, $row;
-    }
-    sort @res;
-}
 
 sub _diff_column_schema {
     my ($c1, $c2) = @_;
@@ -434,9 +184,9 @@ sub diff_table_schema {
     #$log->tracef("Comparing table %s vs %s ...", $table1, $table2);
 
     die "Table $table1 in first database does not exist"
-        unless check_table_exists($dbh1, $table1);
+        unless has_table($dbh1, $table1);
     die "Table $table2 in second database does not exist"
-        unless check_table_exists($dbh2, $table2);
+        unless has_table($dbh2, $table2);
     _diff_table_schema($dbh1, $dbh2, $table1, $table2);
 }
 
